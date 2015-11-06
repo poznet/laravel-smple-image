c60dacdf912ee2c025231f90c493825ca44be749
Michal Glajc <pozyjoner.net@gmail.com>
2015-11-06 21:16:39 +0000

    routes an controller

---
+++ Controllers/ImageController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Poznet\PPAL\Controllers;
+
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Helpers\Media;
+use Illuminate\Http\Response;
+
+class ImageController extends Controller
+{
+
+    /**
+     * @param $path
+     * @param Media $media
+     * @return mixed
+     */
+    public function get($path, Media $media)
+    {
+        $img = Image::make($media->getImage($path));
+        return $img->response();
+    }
+
+    /**
+     * @param $width
+     * @param $path
+     * @param Media $media
+     * @return mixed
+     */
+    public function getMin($width, $path, Media $media)
+    {
+        $img = Image::make($media->getImage($path));
+        $img->resize($width, null, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+        Cache::add('img-'.$width.'-'.$path, $img->response(), 7);
+
+
+        return Cache::get('img-'.$width.'-'.$path);
+
+
+    }
+}
